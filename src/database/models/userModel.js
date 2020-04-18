3a4352eca9b3ca57b892ca8bd939b0f94e25c2ca
Silviu <manzursilviu@gmail.com>
2020-04-18 15:22:31 +0300

    added extra fields

--- src/database/models/userModel.js
+++ src/database/models/userModel.js
@@ -5,6 +5,8 @@ const { Schema } = require('mongoose')
 const userSchema = new Schema({
     username: String,
     password: String,
+    phoneNumber: String,
+    email: String,
     position: String,
     randomKey: {
         type: String,
